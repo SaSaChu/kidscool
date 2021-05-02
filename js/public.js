b13b2bf71c15183f782b013ec3d232b03ce0bafa
TeresaChu <tsgo5888@gmail.com>
2021-05-02 12:56:56 +0800

    公版

--- js/public.js
+++ js/public.js
@@ -1,7 +1,12 @@
 $(function() {
 
-  
+  $('.m_header_r').click(function() {
+    $('.m_menu').addClass('menu_open');
+  });
 
+  $('.menuClose').click(function() {
+    $('.m_menu').removeClass('menu_open');
+  });
 });
 
 
