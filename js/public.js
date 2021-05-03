16b015f8850652d74cf9040a379fad8dc50a4d3d
SaSaChu <teresa568328@gmail.com>
2021-05-03 16:59:33 +0800

    知識寶庫1/2

--- js/public.js
+++ js/public.js
@@ -1,5 +1,41 @@
 $(function() {
 
+  // 知識內文banner 
+  $('.show-box').each(function() {
+    let $showBox = $(this)
+    $showBox.attr('data-index', 1)
+    $showBox.attr('data-page', 1)
+
+    let unit = $showBox.data('unit')
+    let $points = $showBox.find('.points')
+    let $box = $showBox.find('.box-container .box')
+
+    // 點點數量, 產生點點
+    let pointsCount = Math.ceil($box.length / unit)
+    for (let i = 0; i < pointsCount; i++) {
+      $points.append($('<label />').click(function() {
+        $showBox.attr('data-page', $(this).index() + 1);
+      }))
+    }
+
+    // 左邊箭頭
+    $showBox.find('.left').click(function() {
+      let page = parseInt($showBox.attr('data-page'), 10) - 1
+      $showBox.attr('data-page', page < 1 ? pointsCount : page);
+    })
+    // 右邊箭頭
+    $showBox.find('.right').click(function() {
+      let page = parseInt($showBox.attr('data-page'), 10) + 1
+      $showBox.attr('data-page', page > pointsCount ? 1 : page);
+    })
+
+    // 點擊小框框
+    $box.click(function() {
+      $showBox.attr('data-index', $(this).index() + 1)
+    })
+  });
+
+
   $('.m_header_r').click(function() {
     $('.m_menu').addClass('menu_open');
   });
@@ -8,9 +44,6 @@ $(function() {
     $('.m_menu').removeClass('menu_open');
   });
 
-  // $('.m-menu-t').click(function() {
-  //   $('.m-smenu').toggleClass ('m-smenu-show');
-  // });
 
   $('.block-l-menuBox').each (function () {
     var $that = $(this);
@@ -18,6 +51,74 @@ $(function() {
       $(this).toggleClass ('s');
     });
   });
+
+
+
+  // 切換tab(上面) - 五個顏色
+  // 綠色
+  $('.knowBtn.btn_green').click(function() {
+    $('.knowBtn.btn_green').removeClass ('green_s');
+    $(this).addClass ('green_s');
+
+    $('.knowselect').removeClass('knowselect_show');
+    $('.knowselect').eq($(this).index()).addClass('knowselect_show');
+  });
+  $('.knowBtn.btn_green').eq (0).click();
+
+  // 藍色
+  $('.knowBtn.btn_blue').click(function() {
+    $('.knowBtn.btn_blue').removeClass ('blue_s');
+    $(this).addClass ('blue_s');
+
+    $('.knowselect').removeClass('knowselect_show');
+    $('.knowselect').eq($(this).index()).addClass('knowselect_show');
+  });
+  $('.knowBtn.btn_blue').eq (0).click();
+
+  // 紫色
+  $('.knowBtn.btn_purple').click(function() {
+    $('.knowBtn.btn_purple').removeClass ('purple_s');
+    $(this).addClass ('purple_s');
+
+    $('.knowselect').removeClass('knowselect_show');
+    $('.knowselect').eq($(this).index()).addClass('knowselect_show');
+  });
+  $('.knowBtn.btn_purple').eq (0).click();
+
+  // 橘色
+  $('.knowBtn.btn_orange').click(function() {
+    $('.knowBtn.btn_orange').removeClass ('orange_s');
+    $(this).addClass ('orange_s');
+
+    $('.knowselect').removeClass('knowselect_show');
+    $('.knowselect').eq($(this).index()).addClass('knowselect_show');
+  });
+  $('.knowBtn.btn_orange').eq (0).click();
+
+  // 棕色
+  $('.knowBtn.btn_brown').click(function() {
+    $('.knowBtn.btn_brown').removeClass ('brown_s');
+    $(this).addClass ('brown_s');
+
+    $('.knowselect').removeClass('knowselect_show');
+    $('.knowselect').eq($(this).index()).addClass('knowselect_show');
+  });
+  $('.knowBtn.btn_brown').eq (0).click();
+
+
+
+  // 切換tab(左邊)
+  $('.li_green').click(function() {
+    $('.li_green').removeClass ('li_green_s');
+    $(this).addClass ('li_green_s');
+
+    $('.knowBoxsRight').removeClass('knowBoxsRight_show');
+    $('.knowBoxsRight').eq($(this).index()).addClass('knowBoxsRight_show');
+  });
+
+  $('.li_green').eq (0).click();
+
+
 
   //首頁手機・輪播圖
     var curIndex=0;//初始化
@@ -83,7 +184,7 @@ $(function() {
         }else{
           tabBtn[j].classList.remove("hover");//去除輪播按鈕hover樣式
         }
-      }  
+      }
     }
 });
 
