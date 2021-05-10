9c602a8a23cd13341fd73f4937b5a046c3a18b30
SaSaChu <teresa568328@gmail.com>
2021-05-10 21:01:22 +0800

    互動學員一半

--- js/public.js
+++ js/public.js
@@ -263,6 +263,83 @@ $(function() {
         }
       }
     }
+
+    // 互動學員 banner1
+    $('.banner1').each(function() {
+      let $banner = $(this)
+
+      let $bottom = $banner.find('.bottom').empty()
+
+      let total = $banner.find('.box').length
+      let MaxPage = Math.ceil(total / 3)
+
+      let getPage = n => parseInt($banner.attr('p'), 10) + n
+      let setPage = p => $banner.attr('p', p)
+
+      $banner.find('.left').click(_ => {
+        let p = getPage(-1)
+        setPage(p < 1 ? MaxPage : p)
+      })
+      $banner.find('.right').click(_ => {
+        let p = getPage(+1)
+        setPage(p > MaxPage ? 1 : p)
+      })
+
+      for (let i = 0; i < MaxPage; i++)
+        $bottom.append($('<label />').click(function() {
+          setPage($(this).index() + 1)
+        }))
+
+      setPage(1)
+    })
+
+    // 互動學員 banner2
+    $('.banner2').each(function() {
+      let $banner = $(this)
+
+      let $prev = $banner.find('.prev')
+      let $next = $banner.find('.next')
+      let $input = $banner.find('input')
+
+      let total = $banner.find('.box').length
+      let MaxPage = Math.ceil(total / 6)
+
+      let getPage = n => parseInt($banner.attr('p'), 10) + n
+      let setPage = p => $input.val($banner.attr('p', p).attr('p'))
+
+      $input.keyup(function() {
+        let p = getPage(0)
+        setPage(isNaN(p) || p > MaxPage || MaxPage < 1 ? 1 : p)
+      })
+
+      $prev.click(_ => {
+        let p = getPage(-1)
+        setPage(p < 1 ? MaxPage : p)
+      })
+      $next.click(_ => {
+        let p = getPage(+1)
+        setPage(p > MaxPage ? 1 : p)
+      })
+
+      $banner.find('.first').click(_ => setPage(1))
+      $banner.find('.last').click(_ => setPage(MaxPage))
+
+      $banner.find('.left').click(_ => $prev.click())
+      $banner.find('.right').click(_ => $next.click())
+
+      $banner.find('.page').text(MaxPage)
+      setPage(1)
+    })
+
+
+
+
+
+
+
+
+
+
 });
 
 
